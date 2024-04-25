8eed4ca1e0f56c1a000507415a1f01fef2e4ebd5
sarah-bowker <sarahbowker00@gmail.com>
2024-04-25 10:45:53 -0700

    Add comments to improve code readability

--- Blast_func.R
+++ Blast_func.R
@@ -1,83 +1,41 @@
 #a function to run the different blast serches from within R
+# Parameters:
+# btype: a string of the blast search, default is blastn
+# dbase: a string of blast data base file name/path
+# qry: a file of the query sequence
+# Returns:
+# a data frame of the blast search 
 blstinr <- function(btype = "blastn", dbase,qry,numt=1,...){
   
+  # Define the column names for the BLAST output
   colnames <- c("qseqid","sseqid","pident","length","mismatch","gapopen","qstart",
                 "qend","sstart","send","evalue","bitscore")
   
+  # Check the path to the BLAST executable
   bt <- Sys.which(paste(btype))
   
+  # if BLAST executable path was not found, throw an error
   if(nchar(bt) == 0){
     stop(paste("Can't find",btype,
                "on the computer, make sure balast suite is properly installed",
-               sep = " "))} else{if(nchar(bt)>0){
-                 
+               sep = " "))} 
+  # If BLAST executable path found, execute the BLAST command
+  else{if(nchar(bt)>0){
+                  
+                 # Run BLAST search using system2 command
                  bl_out <-system2(command = paste(bt), 
                                   args = c("-db", paste(dbase),
                                            "-query", paste(qry),
                                            "-outfmt", "6",
                                            "-num_threads", paste(numt)), 
                                   wait = TRUE, stdout = TRUE) %>% 
-                   as_tibble() %>% 
-                   separate(col = 1, into = colnames,sep = "\t",
+                   as_tibble() %>%  # form a tibble data frame from the tabular output
+                   separate(col = 1, into = colnames,sep = "\t", # Separate a single column into multiple columns 
                             convert = TRUE) %>% 
-                   mutate(Range = send - sstart)}
-                
-               }  
+                   mutate(Range = send - sstart)} # add a new column, Range, which represents the length of the alignment  
+  }
+  
+  # Return BLAST output
   return(bl_out)
-<<<<<<< HEAD
 }
 
-
-#a function to retrieve the hit sequence from blast search results from within R
-retrieve_hit_seqs <- function(query_ids, blast_results, blastdb, outfile) 
-{
-  ###hit_sequences <- list()
-  output_lines <- character()
-  for (query_id in query_ids) {
-    
-    query_results <- blast_results[blast_results$qseqid == query_id, ]
-    hitSeq <- query_results$sseqid[1]
-    
-    hit_sequence <- system2(
-      command = "blastdbcmd",
-      args = c("-db", blastdb, "-entry", hitSeq),
-      stdout = TRUE,
-      wait = TRUE
-    )
-    output_lines <- c(output_lines, paste(query_id, hitSeq, sep = "__"))
-    
-    output_lines <- c(output_lines, hit_sequence)
-    
-  }
-  
-  writeLines(output_lines, con = outfile)
-  
-  return(output_lines)
-  #return(hit_sequences)
-}
-
-#a function to make blast data given the fasta file from within R
-make_blast_db <- function(infile, dbtype = "nucl", outfile = NULL) 
-{
-  # Check if output file name is provided
-  if (length(outfile) == 0) {
-    stop("Output file name must be included.")
-  }
-  
-  result <- system2(
-    command = "makeblastdb",
-    args = c("-in", infile,
-             "-dbtype", dbtype,
-             "-out", outfile,
-             "-parse_seqids"),
-    stdout = TRUE,
-    wait = TRUE
-  )
-  
-}
-
-
-
-=======
-  }
->>>>>>> c32facfd780f1ef9dfc85ac98cc212d1551212d5
--- make_blast_db.R
+++ make_blast_db.R
@@ -1,21 +1,49 @@
-#a function to make blast data given the fasta file from within R
-#default database type is nucleotide. 
-make_blast_db <- function(infile = file.choose(), dbtype = "nucl", outfile = NULL) {
+# a function to make blast data base given the fasta file from within R
+# Parameters:
+# infile: input file name containing sequences, if not provided, 
+#         a file dialog box is formed to allow the user to select an input file
+# dbtype: a string of data base type, default is nucl
+# outfile: output file name, if not provided the function removes .fa .fasta or .txt 
+#          from the input file and uses it for the output name 
+# taxids_file: a taxonomy information file, expected text file,
+#        if added the function uses it to add these information when forming the data base
+# Returns:
+# a message confirming the success of data base formation 
+# or an error message if data base formation was not successful 
+make_blast_db <- function(infile = file.choose(), dbtype = "nucl", outfile = NULL, taxids_file = NULL) {
   # Check if output file name is provided
-  if(is.null(outfile))
-  {
+  if (is.null(outfile)) {
     outfile <- gsub("\\.[^.]*$", "", infile)
   }
   
-  result <- system2(
-    command = "makeblastdb",
-    args = c("-in", infile,
-             "-dbtype", dbtype,
-             "-out", outfile,
-             "-parse_seqids"),
-    stdout = TRUE,
-    wait = TRUE
-  )
-  print(paste("Blast databae was sucssesful. Outfile name: ", outfile))
+  # Form the command for makeblastdb to be passed to system2 
+  cmd <- c("makeblastdb", "-in", infile, "-dbtype", dbtype, "-out", outfile, "-parse_seqids")
   
+  #Add taxid_map option to the command if taxid_file is provided
+  if (!is.null(taxids_file)) {
+    cmd <- c(cmd, "-taxid_map", taxids_file)
+  }
+  
+  # Execute system command and capture output
+  output <- capture.output({
+    system2(command = cmd, stdout = TRUE, wait = TRUE)
+  })
+  
+  
+  # Filter out the lines containing an error message
+  error_lines <- grep("error:", output, value = TRUE)
+  
+  # Create a character vector to store messages
+  msg <- character()
+  
+  # Store the output message to be returned 
+  if (length(error_lines) > 0) {
+    msg <- c(msg, paste(error_lines))
+  } else {
+    msg <- c(msg, "Blast database successfully created.", paste("Outfile name:", outfile))
+  }
+  
+  # Return the message
+  return(msg)
 }
+
